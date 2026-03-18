ba1725f576ecc58a47b9cfd8e0c54b6223be73be
Joaopmaia-DEV <csgofreemarquin2@gmail.com>
2026-03-18 14:16:44 -0300

    Implement initial book listing and search logic using arrays and switch

--- src/com/joaopaulo/minilibrary/Main.java
+++ src/com/joaopaulo/minilibrary/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         System.out.println("=== MINI LIBRARY LIST===");
 
-        System.out.println("\n=== INITIAL BOOK LIST===");
+        System.out.println("\n=== INITIAL BOOK LIST===\n");
 
         byte[] book = new byte[3];
 
@@ -15,14 +15,32 @@ public class Main {
 
         short[] year = new short[]{2018, 1899, 1605, 1615};
 
-        String[] yearDonQuixote = new String[]{"Part 01: ", "Part 02: "};
+        String[] yearDonQuixote = new String[]{"Part 00: ","Part 01: ", "Part 02: " };
 
         for (int i = 0; i < book.length; i++) {
 
             if (i == 2) {
-                System.out.println("Book " + (i+1) + "\nTitle: " + title[i] + "\nAuthor: "+ authorBook[i]+"\nYear(s):\n"+yearDonQuixote[0] +year[2]+ "\n"+yearDonQuixote[1] + year[3]);
+                System.out.println("Book " + (i+1) + "\nTitle: " + title[i] + "\nAuthor: "+ authorBook[i]+"\nYear(s):\n"+yearDonQuixote[0] +year[2]+ "\n"+yearDonQuixote[1] + year[3]+"\n");
 
-            } else System.out.println("Book " + i + "\nTitle: " + title[i] + "\nAuthor: "+ authorBook[i]+"\nYear: "+year[i]);
+            } else System.out.println("Book " + i + "\nTitle: " + title[i] + "\nAuthor: "+ authorBook[i]+"\nYear: "+year[i]+"\n");
+        }
+        System.out.println("\n=== SEARCH BOOK ===\n");
+        for (int i = 0; i < book.length; i++) {
+            switch(title[i]){
+                case "Dom Casmurro":
+                    System.out.println("Searching for title: "+ title[i]+"\n\n---------------\nBook found\nTitle: "+title[i]+"\nAuthor: "+ authorBook[i]+"\nYear: "+year[i]+"\n---------------\n");
+                    break;
+                case "Atomic Habits":
+                    System.out.println("Searching for title: "+ title[i]+"\n\n---------------\nBook found\nTitle: "+title[i]+"\nAuthor: "+ authorBook[i]+"\nYear: "+year[i]+"\n---------------\n");
+                    break;
+                case "Don Quixote":
+                    System.out.println("Searching for title: "+ title[i]+"\n\n---------------\nBook found\nTitle: "+title[i]+"\nAuthor: "+ authorBook[i]+"\nYear(s):\n"+yearDonQuixote[i-1]+ year[i]+"\n"+yearDonQuixote[i]+year[i+1]+"\n---------------\n");
+                    break;
+
+
+
+
+            }
         }
     }
 }
