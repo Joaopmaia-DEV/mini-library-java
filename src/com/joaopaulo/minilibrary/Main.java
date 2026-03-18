4244305a63f767e69a2349433d4bf20db179f1db
Joaopmaia-DEV <csgofreemarquin2@gmail.com>
2026-03-17 22:07:41 -0300

    Add initial library system with book listing and basic conditional logic

--- src/com/joaopaulo/minilibrary/Main.java
+++ src/com/joaopaulo/minilibrary/Main.java
@@ -1,4 +1,28 @@
 package com.joaopaulo.minilibrary;
 
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("=== MINI LIBRARY LIST===");
+
+        System.out.println("\n=== INITIAL BOOK LIST===");
+
+        byte[] book = new byte[3];
+
+        String[] authorBook = new String[]{"James Clear", "Machado de Assis", "Miguel de Cervantes"};
+
+        String[] title = new String[]{"Atomic Habits", "Dom Casmurro", "Don Quixote"};
+
+        short[] year = new short[]{2018, 1899, 1605, 1615};
+
+        String[] yearDonQuixote = new String[]{"Part 01: ", "Part 02: "};
+
+        for (int i = 0; i < book.length; i++) {
+
+            if (i == 2) {
+                System.out.println("Book " + (i+1) + "\nTitle: " + title[i] + "\nAuthor: "+ authorBook[i]+"\nYear(s):\n"+yearDonQuixote[0] +year[2]+ "\n"+yearDonQuixote[1] + year[3]);
+
+            } else System.out.println("Book " + i + "\nTitle: " + title[i] + "\nAuthor: "+ authorBook[i]+"\nYear: "+year[i]);
+        }
+    }
 }
